89cf4d2d281d1d5dfd098982c7e9eee74827ac5b
puneeth-grinds <puneethm2736@gmail.com>
2026-08-25 04:57:36 +0000

    GoLang

--- go/day96/main.go
+++ go/day96/main.go
@@ -1,11 +1,15 @@
+data := []byte(`{
+  "repository": "github.com/mycompany/payment-service",
+  "branch": "main"
+}`)
 type ScanRequest struct {
 	Repository string 
 	Branch string
 }
 var scanRequest ScanRequest 
-res, err := json.Unmarshal(data, &scanRequest)
+err := json.Unmarshal(data, &scanRequest)
 if err != nil {
 	fmt.Println(err)
 	return
 }
-fmt.Println(ScanRequest.Repository, scanRequest.Branch)
+fmt.Println(scanRequest.Repository, scanRequest.Branch)
