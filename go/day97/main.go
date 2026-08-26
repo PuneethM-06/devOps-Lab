6fb2de426d2183ff48fa86a96efe57d083a43f07
puneeth-grinds <puneethm2736@gmail.com>
2026-08-26 05:32:53 +0000

    GoLang

--- go/day97/main.go
+++ go/day97/main.go
@@ -1,29 +1,7 @@
-
-func handler(w http.ResponseWriter, r *http.Request){
-	if r.Method != "GET"{
-		fmt.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
-	}
-	fmt.FPrintln(w, "Hello from Server")
-}
-
-http.HandleFunc("/", handler)
-http.HandleFunc("/health", healthHandler)
-http.ListenAndServe(":8080", nil)
-
-func healthHandler(w http.ResponseWriter, r *http.Request){
-	fmt.FPrintln(w, "OK")
-	fmt.Println(r.Method)
-}
-
-if err != nil {
-	fmt.Println(err)
-	return
-}
-
-res, err := io.ReadAll(resp.Body)
-fmt.Println(string(res))
-
-if resp.StatusCode != 200 {
-
-}
+package main 
+import (
+	"fmt"
+	"net/http"
+)
+http.handleFunc("/scan", handler)
+http.ListenAndServe(":8080", nil )
